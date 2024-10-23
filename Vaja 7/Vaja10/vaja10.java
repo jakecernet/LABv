bfd6f98972613ba9160826893e638326b7391fa4
Jaka Černetič <jaka.cernetic@gmail.com>
2024-10-23 14:36:43 +0200

    popravki

--- Vaja 7/Vaja10/vaja10.java
+++ Vaja 7/Vaja10/vaja10.java
@@ -28,7 +28,8 @@ public class vaja10 {
             }
             System.out.println();
         }
-        for (int i = n - 2; i >= 0; i--) {
+
+        for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j > i; j--) {
                 System.out.print("  ");
             }
--- Vaja 7/Vaja8/vaja8.java
+++ Vaja 7/Vaja8/vaja8.java
@@ -1,15 +1,18 @@
 /* Realizirajte tudi (glej besedilo predhodne naloge):
-0
-1 0
-2 1 0
-3 2 1 0
-4 3 2 1 0
+          0
+        1 0
+      2 1 0
+    3 2 1 0
+  4 3 2 1 0
 5 4 3 2 1 0 */
 
 public class vaja8 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print("  ");
+            }
             for (int j = i; j >= 0; j--) {
                 System.out.print(j + " ");
             }
